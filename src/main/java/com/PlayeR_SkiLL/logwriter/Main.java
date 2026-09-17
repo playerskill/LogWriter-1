880c1e952985c39e6fee8e2f92aa2210d8d72fba
playerskill <96878065+playerskill@users.noreply.github.com>
2026-09-18 01:39:48 +0300

    Create Main.java

---
+++ src/main/java/com/PlayeR_SkiLL/logwriter/Main.java
@@ -0,0 +1,62 @@
+package com.PlayeR_SkiLL.logwriter;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import com.PlayeR_SkiLL.logwriter.commands.LogWriteCommand;
+import com.PlayeR_SkiLL.logwriter.commands.LogWriterCommand;
+import com.PlayeR_SkiLL.logwriter.commands.LwCommand;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+    private FileConfiguration messages;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        // Сохраняем только messages.yml
+        saveResource("messages.yml", false);
+        createLogFilesFolder();
+
+        // Регистрация команд
+        getCommand("logwrite").setExecutor(new LogWriteCommand());
+        getCommand("logwriter").setExecutor(new LogWriterCommand());
+        getCommand("lw").setExecutor(new LwCommand());
+
+        // Загрузка сообщений
+        loadMessages();
+    }
+
+    @Override
+    public void onDisable() {
+        // ничего не требуется
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public void reloadPlugin() {
+        saveResource("messages.yml", false);
+        loadMessages();
+    }
+
+    public void createLogFilesFolder() {
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
+        java.io.File logFolder = new java.io.File(getDataFolder(), "logfiles");
+        if (!logFolder.exists()) {
+            logFolder.mkdirs();
+        }
+    }
+
+    private void loadMessages() {
+        this.messages = org.bukkit.configuration.file.YamlConfiguration.loadConfiguration(new java.io.File(getDataFolder(), "messages.yml"));
+    }
+
+    public FileConfiguration getMessages() {
+        return messages;
+    }
+}
