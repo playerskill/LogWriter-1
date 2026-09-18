c75bde0792e79113fc2177d479c12749d517a593
playerskill <96878065+playerskill@users.noreply.github.com>
2026-09-18 12:05:13 +0300

    Update LogWriter.java

--- src/main/java/com/PlayeR_SkiLL/logwriter/LogWriter.java
+++ src/main/java/com/PlayeR_SkiLL/logwriter/LogWriter.java
@@ -2,15 +2,41 @@ package com.PlayeR_SkiLL.LogWriter;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class LogWriter extends JavaPlugin {
+
     @Override
     public void onEnable() {
         getLogger().info("LogWriter включен!");
-        // Инициализация команд и т.п.
+
+        // Создаем папки при запуске
+        createLogFolder();
+
+        // Регистрация команд и других инициализаций
+        // например: getCommand("logwriter").setExecutor(new LogWriterCommand(this));
     }
 
     @Override
     public void onDisable() {
         getLogger().info("LogWriter выключен!");
+    }
+
+    private void createLogFolder() {
+        // Получаем папку плагина
+        File pluginFolder = getDataFolder();
+
+        // Создаем папку logfiles внутри папки плагина
+        File logFolder = new File(pluginFolder, "logfiles");
+        if (!logFolder.exists()) {
+            boolean success = logFolder.mkdirs();
+            if (success) {
+                getLogger().info("[LogWriter] Папка logfiles успешно создана");
+            } else {
+                getLogger().warning("[LogWriter] Не удалось создать папку logfiles");
+            }
+        } else {
+            getLogger().info("[LogWriter] Папка logfiles уже существует");
+        }
     }
 }
